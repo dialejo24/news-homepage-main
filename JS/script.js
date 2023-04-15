e99c3fe75d2105bc0ef55caed51b08345450e646
Dialejo24 <dialejosi530@gmail.com>
2023-04-15 15:44:00 -0500

    Complete mobile window

--- JS/script.js
+++ JS/script.js
@@ -7,8 +7,14 @@ let transparentSection = document.querySelector(".transparent-section");
 iconMenu.addEventListener("click", displayMobileWindow);
 iconClose.addEventListener("click", hideMobileWindow);
 transparentSection.addEventListener("click", hideMobileWindow) //hide the window if user clicks (touches) the transparent section
+window.addEventListener("resize", () => {
+    //hides the window if it's activated and user resizes the screen such that the screen's size is >= 800
+    if (document.body.clientWidth >= 800 && mobileWindow.classList.contains("showWindow")) {
+        hideMobileWindow();
+    } 
+});
 
-function displayMobileWindow(e) {
+function displayMobileWindow() {
     mobileWindow.classList.remove("hideWindow");
     mobileWindow.classList.add("showWindow");
 
@@ -19,7 +25,7 @@ function displayMobileWindow(e) {
     document.body.parentElement.style.overflowY = "hidden";
 }
 
-function hideMobileWindow(e) {
+function hideMobileWindow() {
     mobileWindow.classList.remove("showWindow");
     mobileWindow.classList.add("hideWindow");
 
